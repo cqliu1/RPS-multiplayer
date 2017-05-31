3d567a862af8d38f2c111871c047094d1a738147
Catherine Liu <catherine@Catherines-MacBook-Pro.local>
2017-05-30 20:26:10 -0700

    Added firebase logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,8 @@ $(document).ready(function(){
 
 	var thisPlayer = "";
 
+	var status = "";
+
 	var p1WinsSpan = $("#player1 .wins");
 	var p2WinsSpan = $("#player2 .wins");
 	var tiesSpan = $("#ties");
@@ -46,6 +48,28 @@ $(document).ready(function(){
 		updateScores();
 	}
 
+	function resetGame() {
+		p1Selection = "";
+		p1Wins = 0;
+
+		p2Selection = "";
+		p2Wins = 0;
+
+		ties = 0;
+
+		thisPlayer = "";
+
+		p1Online = false;
+		p2Online = false;
+
+		database.ref().update({
+			status: "Click join to start a new game!"
+		});
+		$(".join").show().removeClass("disabled");
+		$(".selection").removeClass("disabled");
+		$(".player-select").hide();
+	}
+
 	function rps(selection1, selection2) {
 
 		// check which player wins
@@ -56,35 +80,56 @@ $(document).ready(function(){
 			if(selection2 === "p") {
 				p2Wins++;
 				$("#status").text("Player 2 wins!");
+				database.ref().update({
+					p2Wins: p2Wins,
+					status: "Player 2 wins!"
+				});
 			}
 			else if(selection2 === "s") {
 				p1Wins++;
-				$("#status").text("Player 1 wins!");
+				database.ref().update({
+					p1Wins: p1Wins,
+					status: "Player 1 wins!"
+				});
 			}
 		} else if(selection1 === "p") {
 			if(selection2 === "r") {
 				p1Wins++;
-				$("#status").text("Player 1 wins!");
+				database.ref().update({
+					p1Wins: p1Wins,
+					status: "Player 1 wins!"
+				});
 			}
 			else if(selection2 === "s") {
 				p2Wins++;
-				$("#status").text("Player 2 wins!");
+				database.ref().update({
+					p2Wins: p2Wins,
+					status: "Player 2 wins!"
+				});
 			}
 		} else if(selection1 === "s") {
 			if(selection2 === "r") {
 				p2Wins++;
-				$("#status").text("Player 2 wins!");
+				database.ref().update({
+					p2Wins: p2Wins,
+					status: "Player 2 wins!"
+				});
 			}
 			else if(selection2 === "p") {
 				p1Wins++;
-				$("#status").text("Player 1 wins!");
+				database.ref().update({
+					p1Wins: p1Wins,
+					status: "Player 1 wins!"
+				});
 			}
 		}
 
 		database.ref().update({
 			p1Wins: p1Wins,
 			p2Wins: p2Wins,
-			ties: ties
+			ties: ties,
+			p1Selection: "",
+			p2Selection: ""
 		});
 	}
 
@@ -107,6 +152,7 @@ $(document).ready(function(){
 				database.ref().set({
 					p1Online: true,
 					p1Wins:0,
+					p1Selection:"",
 					ties:0
 				});
 
@@ -118,7 +164,8 @@ $(document).ready(function(){
 
 				database.ref().set({
 					p2Online: true,
-					p2Wins:0
+					p2Wins:0,
+					p1Selection:""
 				});
 
 				$("#player2 .player-select").show();
@@ -131,28 +178,13 @@ $(document).ready(function(){
 		}	
 	});
 
-	$(".leave").on("click", function() {
-		// clears all game data when either player leaves the game
-		database.ref().remove();
+	// // when player leaves the page
+	// $(window).on("unload", function() {
+	// 	// clears all game data when either player leaves the game
+	// 	database.ref().remove();
 
-		p1Selection = "";
-		p1Wins = 0;
-
-		p2Selection = "";
-		p2Wins = 0;
-
-		ties = 0;
-
-		thisPlayer = "";
-
-		p1Online = false;
-		p2Online = false;
-
-		$("#status").text("Game over! Click join to start a new game!");
-		$(".join").show().removeClass("disabled");
-		$(".selection").removeClass("disabled");
-		$(".player-select").hide();
-	});
+	// 	resetGame();
+	// });
 
 	database.ref().on('value', function(snapshot) {
 		console.log(snapshot.val());
@@ -165,26 +197,50 @@ $(document).ready(function(){
 			p2Online = data.p2Online;
 			p2Wins = data.p2Wins;
 			ties = data.ties;
+			status = data.status;
+			p1Selection = data.p1Selection;
+			p2Selection = data.p2Selection;
 		}
 
-		if(p1Online && thisPlayer === "1") {
+		console.log("selections:",p1Selection,p2Selection);
+
+		if(p1Selection !== undefined && p2Selection !== undefined && p1Selection !== "" && p2Selection !== "") {
+			rps(p1Selection,p2Selection);
+		}
+
+		if(p1Online) {
+			console.log("player 1 exists");
 			$("#player1 .join").hide();
-			$(".join").addClass("disabled");
 			$("#player1 .player-select").show();
 			$("#player1 .score").show();
+
+			if(thisPlayer === "1"){
+				$(".join").addClass("disabled");
+			} else {
+				$("#player1 .selection").addClass("disabled");
+			}
 		}
 
-		if(p2Online && thisPlayer === "2") {
+		if(p2Online) {
+			console.log("player 2 exists");
 			$("#player2 .join").hide();
-			$(".join").addClass("disabled");
 			$("#player2 .player-select").show();
 			$("#player2 .score").show();
+
+			if(thisPlayer === "2"){
+				$(".join").addClass("disabled");
+			} else {
+				$("#player2 .selection").addClass("disabled");
+			}
 		}
 
 		updateScores();
+		$("#status").text(status);
 
 		if(p1Online && p2Online) {
 			$(".score").show();
+		} else {
+			resetGame();
 		}
 	});
 
